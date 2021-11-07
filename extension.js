b0edd3fbcbf8efd390137bc7cf246da39d39202a
r4nd0wn <jonas@jbach.xyz>
2021-11-07 22:21:54 +0100

    too lazy to commit properly sry m8s

--- extension.js
+++ extension.js
@@ -18,9 +18,14 @@
 
 /* exported init */
 
-const {St, GLib, GObject, Clutter} = imports.gi;
+const {St, GLib, GObject, Clutter, Gio, Meta, Shell} = imports.gi;
 const Main = imports.ui.main;
+const Mainloop = imports.mainloop;
+const Me = imports.misc.extensionUtils.getCurrentExtension();
 
+
+
+//const Keybinder = imports.Keybinder;
 let panelButton, panelButtonText;
 
 function init () {
@@ -32,9 +37,64 @@ function init () {
     text : "Hello world",
   });
   panelButton.set_child(panelButtonText);
-  log('active ws index');
-  this.active_ws_index = global.workspace_manager.get_active_workspace_index();
-  log(this.active_ws_index);
+  log("Starting WS: ", this.active_ws_index);
+  wsm = new WS_manager();
+
+}
+
+
+function _get_currentws(){
+  ws = global.workspace_manager.get_active_workspace_index();
+  log(ws)
+}
+
+
+
+class WS_manager{
+  // Class to manage Workspace switches. should always remember the past Workspace.
+  constructor(){
+    log("WS Manager running");
+    this.current_ws = null;
+    this.last_ws = null;
+
+
+    let handler_id = global.workspace_manager.connect('workspace-switched', (objectidc, last_ws, current_ws) => {
+      this.current_ws = current_ws;
+      this.last_ws = last_ws;
+
+      this.current_ws = global.workspace_manager.get_active_workspace_index();
+      log(`ws switched from ${this.last_ws} to ${this.current_ws}`);
+      log(typeof(this.last_ws));
+    })
+
+    Main.wm.addKeybinding("my-shortcut", getSettings(), Meta.KeyBindingFlags.NONE, Shell.ActionMode.ALL, () => {
+      this._switch_ws(this.last_ws);
+    });
+
+  }
+
+  _switch_ws(to){
+    // wrapper for global switch Workspace.
+    global.workspace_manager.get_workspace_by_index(to).activate(global.get_current_time());
+    //this.last_ws.activate(global.get_current_time());
+    log(`switched WS from ${current_ws} to ${to}`);
+  }
+
+}
+
+function getSettings () {
+  let GioSSS = Gio.SettingsSchemaSource;
+  let schemaSource = GioSSS.new_from_directory(
+    Me.dir.get_child("schemas").get_path(),
+    GioSSS.get_default(),
+    false
+  );
+  let schemaObj = schemaSource.lookup(
+    'org.gnome.shell.extensions.workspaceswitcher', true);
+  if (!schemaObj) {
+    throw new Error('cannot find schemas');
+  }
+  return new Gio.Settings({ settings_schema : schemaObj });
 }
 
 function enable () {
@@ -42,5 +102,9 @@ function enable () {
 }
 
 function disable () {
+  let shellwm = global.workspace_manager
+  let handler = GObject.signal_handler_find(shellwm, {signalId: 'workspace-switched'});
+  shellwm.disconnect(handler);
+
   Main.panel._rightBox.remove_child(panelButton);
 }
